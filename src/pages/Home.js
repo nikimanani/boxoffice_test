87dbf002a86e1c582c388456a87456e7ff99afac
Niki Manani <nikisuccess.deore402@gmail.com>
2021-07-22 22:51:25 +0530

    API

---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+
+const Home = () => {
+
+    const [input, setInput] = useState('');
+
+
+    const onSearch = () => {
+        fetch(`http://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
+            console.log(result);
+        })
+    };
+
+    const onInputChange = ev => {
+        setInput(ev.target.value);
+        console.log(ev.target.value);
+    };
+
+    const onKeyDown = (ev) => {
+        if (ev.keyCode === 13) {
+            onSearch();
+        };
+
+    }
+
+    return <MainPageLayout >
+        <
+        input type = "text"
+    onChange = { onInputChange }
+    onKeyDown = { onKeyDown }
+    value = { input }
+    /> <
+    button type = "button"
+    onClick = { onSearch } > Search < /button> <
+        /MainPageLayout>
+};
+
+export default Home;
