fc3657395ad6a5552574bd9cc0d9920b7cb8d6b6
Niki Manani <nikisuccess.deore402@gmail.com>
2021-07-28 17:23:52 +0530

    full

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,105 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
+import { useLastQuery } from '../misc/custom-hooks';
+import {
+    SearchInput,
+    RadioInputsWrapper,
+    SearchButtonWrapper,
+} from './Home.styled';
+import CustomRadio from '../components/CustomRadio';
 
 const Home = () => {
+    const [input, setInput] = useLastQuery();
+    const [results, setResults] = useState(null);
+    const [searchOption, setSearchOption] = useState('shows');
 
-    const [input, setInput] = useState('');
-
-
+    const isShowsSearch = searchOption === 'shows';
     const onSearch = () => {
-        fetch(`http://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
-            console.log(result);
-        })
+        apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+            setResults(result);
+        });
     };
 
     const onInputChange = ev => {
         setInput(ev.target.value);
-        console.log(ev.target.value);
     };
 
-    const onKeyDown = (ev) => {
+    const onKeyDown = ev => {
         if (ev.keyCode === 13) {
             onSearch();
-        };
+        }
+    };
 
-    }
+    const onRadioChange = ev => {
+        setSearchOption(ev.target.value);
+    };
 
-    return <MainPageLayout >
+    const renderResults = () => {
+        if (results && results.length === 0) {
+            return <div > No results < /div>;
+        }
+
+        if (results && results.length > 0) {
+            return results[0].show ? ( <
+                ShowGrid data = { results }
+                />
+            ) : ( <
+                ActorGrid data = { results }
+                />
+            );
+        }
+
+        return null;
+    };
+
+    return ( <
+        MainPageLayout >
         <
-        input type = "text"
-    onChange = { onInputChange }
-    onKeyDown = { onKeyDown }
-    value = { input }
-    /> <
-    button type = "button"
-    onClick = { onSearch } > Search < /button> <
+        SearchInput type = "text"
+        placeholder = "Search for something"
+        onChange = { onInputChange }
+        onKeyDown = { onKeyDown }
+        value = { input }
+        />
+
+        <
+        RadioInputsWrapper >
+        <
+        div >
+        <
+        CustomRadio label = "Shows"
+        id = "shows-search"
+        value = "shows"
+        checked = { isShowsSearch }
+        onChange = { onRadioChange }
+        /> <
+        /div>
+
+        <
+        div >
+        <
+        CustomRadio label = "Actors"
+        id = "actors-search"
+        value = "people"
+        checked = {!isShowsSearch }
+        onChange = { onRadioChange }
+        /> <
+        /div> <
+        /RadioInputsWrapper>
+
+        <
+        SearchButtonWrapper >
+        <
+        button type = "button"
+        onClick = { onSearch } >
+        Search <
+        /button> <
+        /SearchButtonWrapper> { renderResults() } <
         /MainPageLayout>
+    );
 };
 
 export default Home;
